5a553e64c7713217e28dea1353c527b5dc859680
barrenmoor <moorbarren@gmail.com>
2022-08-09 16:50:21 +0530

    test vpc creation

--- main.tf
+++ main.tf
@@ -10,36 +10,9 @@ provider "aws" {
 
 resource "random_uuid" "randomid" {}
 
-resource "aws_s3_bucket" "app" {
+resource "aws_default_vpc" "default" {
   tags = {
-    Name = "App Bucket"
-  }
-
-  bucket        = "${var.app}.${var.label}.${random_uuid.randomid.result}"
-  force_destroy = true
-}
-
-resource "aws_s3_object" "app" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.app.id
-  content      = file("./assets/index.html")
-  content_type = "text/html"
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_website_configuration" "terramino" {
-  bucket = aws_s3_bucket.app.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
+    Name = "Default VPC - Created from CircleCI"
   }
 }
+
